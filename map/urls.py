31979bed88c4f3f06aec019bf62ef9ce88f5a0d9
dx2331lxz <3434055686@qq.com>
2024-04-09 22:17:15 +0800

    daoxuan 进行的更改

--- map/urls.py
+++ map/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('province/', views.ProvinceAPIView.as_view()),
+    path('light/', views.LightAPIView.as_view()),
+    path('rank/', views.RankAPIView.as_view()),
+    path('verify/', views.GoHomeAPIView.as_view()),
+    path('count/', views.count),
 ]
